2382cd7182196fec06f6a1b1bdf0a9eccc97ad0b
Abayomi Theodora <teojesus.silva@gmail.com>
2023-06-03 08:59:33 -0300

    exercicio2

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicio2.js
@@ -0,0 +1,16 @@
+let num1 = 23;
+let num2 = 356;
+let num3 = 93;
+let result;
+
+if (num1 > num2) {
+    result = num1;
+}
+else if (num2 > num3) {
+    result = num2;
+}
+else if (num3 > num2) {
+    result = num3
+}
+
+console.log(result);
