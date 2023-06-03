dc1c480abfc60d29c1203831e06362159adce509
Abayomi Theodora <teojesus.silva@gmail.com>
2023-06-03 08:43:11 -0300

    exercico.1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/aprendendo_js.js
@@ -0,0 +1,10 @@
+let num1 = 24;
+let num2 = 67;
+
+let sim = num1 + num2;
+let nao = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+
+console.log(sim, nao, mult, div, mod);
