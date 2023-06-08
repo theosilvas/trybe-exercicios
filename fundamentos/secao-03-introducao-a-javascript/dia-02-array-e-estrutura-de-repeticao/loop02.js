38b7a510d2602c47ffc5c519354b16b42137c4ab
Abayomi Theodora <teojesus.silva@gmail.com>
2023-06-08 06:10:53 -0300

    mais um

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/loop02.js
@@ -0,0 +1,9 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somaTotal = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    somaTotal += numbers[index];
+}
+
+console.log(somaTotal);
